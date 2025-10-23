8ee0a22568baad55605ac06c67721c1d01c2d452
redes19 <120016236+redes19@users.noreply.github.com>
2025-10-23 15:23:27 +0200

    .

--- server.js
+++ server.js
@@ -10,6 +10,29 @@ const resolvers = {
             { id: "1",  name: "Alice", age: 25 },
             { id: "2", name: "Bob", age: 30 },
         ],
+        person: async (_, {id}) => {
+            const session = driver.session();
+            try {
+                const result = await session.run(
+                    "MATCH (p: Person {id :$id} RETURN p)",
+                    {id}
+                );
+
+                if(result.records.length === 0) return null;
+
+                const person = result.records[0].get("p");
+                return {
+                    id : person.id,
+                    firstname : person.firstname,
+                    lastname: person.lastname,
+                    name: person.name,
+                    birth: person.birth ? parseInt(person.birth) : null,
+                };
+
+            } finally{
+                await session.close();
+            }
+        }
     },
 };
 
--- shema.js
+++ shema.js
@@ -7,9 +7,18 @@ const typeDefs = gql`
         age: Int
     }
 
+    type Person {
+        id: ID!
+        firstname: String
+        lastname: String
+        name: String
+        birth: Int
+    }
+
     type Query {
         hello : String
         users : [User]
+        person: [Person]
     }
 
 `;
